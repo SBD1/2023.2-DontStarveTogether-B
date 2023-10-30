efdf32bd23afeb3ca6a4848c648ca610f536aef3
Luis Bruno <l.brunofidelis@gmail.com>
2023-10-29 23:55:18 -0300

    Atualiza inserts

--- sql/DML.sql
+++ sql/DML.sql
@@ -1,7 +1,7 @@
 -- Criacao: 26/10/2023
 -- Autores: Luis Bruno
 -- Descrição: Criação dos inserts (DML)
-
+-- ROLLBACK
 BEGIN TRANSACTION;
 
 -- Usuario
@@ -12,6 +12,26 @@ INSERT INTO Usuario (nomeUsuario, nome, email, senha) VALUES ('lukeskywalker', '
 ('joaodasilva', 'João Silva', 'joao@gmail.com', 'senha123');
 
 SELECT * FROM Usuario;
+
+-- TipoMundo
+INSERT INTO TipoMundo (nome, x, y, z) VALUES ('Small', 100, 100, 100),
+       ('Medium', 300, 300, 300),
+       ('Large', 500, 500, 500),
+       ('Huge', 1000, 1000, 1000);
+
+SELECT * FROM TipoMundo;
+
+-- Mundo
+INSERT INTO Mundo (idUsuario, tipo, nome, estacao, temEstrada)
+VALUES ('lukeskywalker', 1, 'Mundo sem nome', 'Autumn', true),
+       ('darthvader', 2, 'World 2', 'Winter', false),
+       ('fulano123', 3, 'Gameplay da galera', 'Spring', true),
+       ('carlosroberto', 4, 'to sem criatividade', 'Summer', false),
+       ('lukeskywalker', 2, 'unnamed world', 'Winter', true),
+       ('darthvader', 3, 'terraria > dont starve', 'Spring', false),
+       ('fulano123', 4, 'lol > dota', 'Summer', true);
+	  
+SELECT * FROM Mundo;
 
 -- Personagem
 INSERT INTO Personagem (nome, descricao, vida) VALUES ('Wilson', 'Cientista', 150),
@@ -115,13 +135,53 @@ INSERT INTO InstanciaNpc (idNpc, x, y, z, vidaAtual) VALUES
 (3, 74, 81, 92, 73),
 (4, 10, 110, 120, 1350); -- Criar check para que o HPAtual não seja maior que o Hp total
 
-SELECT * FROM InstanciaNpc
+SELECT * FROM InstanciaNpc;
+
+-- InstanciaPC
+INSERT INTO InstanciaPC (idPersonagemJogavel, vidaAtual, fomeAtual, sanidadeAtual, modoFantasma, x, y, z) VALUES
+    (1, 95, 100, 60, false, 10, 20, 30),
+    (2, 20, 80, 15, false, 15, 25, 35),
+    (3, 30, 100, 50, true, 12, 18, 32),
+    (4, 49, 90, 75, false, 11, 22, 34);
+
+SELECT * FROM InstanciaPC;
+
+-- Alianca
+INSERT INTO Alianca (idInstanciaPc, idInstanciaNpc) VALUES (4, 9), (3, 1);
+
+SELECT * FROM Alianca;
+
+-- Itens basicos
+INSERT INTO Item (nome, descricao) VALUES
+  ('Madeira', 'Um pedaço de madeira normal'),
+  ('Pedra', 'Uma pedra comum, pode ser usada para construção'),
+  ('Flint', 'Um fragmento de pedra afiada, ótimo para ferramentas'),
+  ('Grama', 'Uma grama comum'),
+  ('Flor', 'Uma bela flor silvestre'),
+  ('Folha', 'Uma folha verde'),
+  ('Carvão', 'Carvão de madeira queimada'),
+  ('Minério de Ferro', 'Minério de ferro bruto'),
+  ('Pena', 'Uma pena de ave'),
+  ('Pele de Animal', 'Uma pele de animal selvagem');
+
+-- Consumiveis 
+INSERT INTO Item (nome, descricao, referenciaItem)
+VALUES
+  ('Cozido de Carne', 'Um prato saboroso feito com carne de animal', NULL),
+  ('Salada de Folhas', 'Uma salada refrescante de folhas verdes', NULL),
+  ('Sopa de Pele', 'Uma sopa quentinha e nutritiva feita com pele de animal', NULL);
+
+SELECT * FROM ITEM;
+
+-- Receita - 1 item
 
 COMMIT;
 
 -- ROLLBACK
 
 -- Alters - p/ resetar os ids
+ALTER SEQUENCE TipoMundo_id_seq RESTART WITH 1;
+ALTER SEQUENCE Mundo_id_seq RESTART WITH 1;
 ALTER SEQUENCE Personagem_id_seq RESTART WITH 1;
 ALTER SEQUENCE PersonagemUsuario_id_seq RESTART WITH 1;
 ALTER SEQUENCE PersonagemJogavel_id_seq RESTART WITH 1;
@@ -131,8 +191,22 @@ ALTER SEQUENCE Dialogo_id_seq RESTART WITH 1;
 ALTER SEQUENCE InstanciaNpc_id_seq RESTART WITH 1;
 ALTER SEQUENCE InstanciaPC_id_seq RESTART WITH 1;
 ALTER SEQUENCE Alianca_id_seq RESTART WITH 1;
+ALTER SEQUENCE Equipamento_id_seq RESTART WITH 1;
+ALTER SEQUENCE EquipamentoPersonagem_id_seq RESTART WITH 1;
+ALTER SEQUENCE Colocavel_id_seq RESTART WITH 1;
+ALTER SEQUENCE Consumivel_id_seq RESTART WITH 1;
+ALTER SEQUENCE Inventario_id_seq RESTART WITH 1;
 
 -- Drops
+DROP TABLE IF EXISTS Inventario CASCADE;
+DROP TABLE IF EXISTS Consumivel CASCADE;
+DROP TABLE IF EXISTS Colocavel CASCADE;
+DROP TABLE IF EXISTS EquipamentoPersonagem CASCADE;
+DROP TABLE IF EXISTS Equipamento CASCADE;
+DROP TABLE IF EXISTS Item CASCADE;
+DROP TABLE IF EXISTS Habilidade CASCADE;
+DROP TABLE IF EXISTS Receita CASCADE;
+DROP TABLE IF EXISTS Item CASCADE;
 DROP TABLE IF EXISTS Alianca CASCADE;
 DROP TABLE IF EXISTS InstanciaPC CASCADE;
 DROP TABLE IF EXISTS InstanciaNpc CASCADE;
@@ -142,4 +216,6 @@ DROP TABLE IF EXISTS Npc CASCADE;
 DROP TABLE IF EXISTS PersonagemJogavel CASCADE;
 DROP TABLE IF EXISTS PersonagemUsuario CASCADE;
 DROP TABLE IF EXISTS Personagem CASCADE;
+DROP TABLE IF EXISTS Mundo CASCADE;
+DROP TABLE IF EXISTS TipoMundo CASCADE;
 DROP TABLE IF EXISTS Usuario CASCADE;
