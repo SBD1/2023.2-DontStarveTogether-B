010d0a314e55ad8068b85e46a0ecd13b1d936c2f
Luis Bruno <l.brunofidelis@gmail.com>
2023-11-19 13:45:33 -0300

    Cria trig/sp p/ verificar especializaç. Personagem

--- sql/Trigger-SP.sql
+++ sql/Trigger-SP.sql
@@ -11,7 +11,7 @@ WHERE event_object_table = 'Inventario'
 GROUP BY table_name, trigger_name 
 ORDER BY table_name, trigger_name
 
-
+-- Trigger que verifica se há espaço na mochila antes de inserir
 CREATE OR REPLACE FUNCTION atualiza_inventario() RETURNS TRIGGER
 AS
 $$
@@ -33,3 +33,40 @@ CREATE TRIGGER t_atualiza_inventario
 BEFORE INSERT ON Inventario
 FOR EACH ROW
 EXECUTE PROCEDURE atualiza_inventario();
+
+-- Triggers/SP para garantir especialização Personagem - PersonagemJogavel - Npc
+CREATE OR REPLACE FUNCTION verificarPersonagemJogavel()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF EXISTS (SELECT 1 FROM Npc WHERE idPersonagem = NEW.idPersonagem) THEN
+        RAISE EXCEPTION 'Personagem já é um Npc';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER t_verificarPersonagemJogavel
+BEFORE INSERT ON PersonagemJogavel
+FOR EACH ROW
+EXECUTE PROCEDURE verificarPersonagemJogavel();
+
+CREATE OR REPLACE FUNCTION verificarNpc()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF EXISTS (SELECT 1 FROM PersonagemJogavel WHERE idPersonagem = NEW.idPersonagem) THEN
+		RAISE EXCEPTION 'Npc já é um personagem jogável';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER t_verificarNpc
+BEFORE INSERT ON Npc
+FOR EACH ROW
+EXECUTE PROCEDURE verificarNpc();
+
+-- Teste das TRIGGERS e Stored Procedure 
+INSERT INTO PersonagemJogavel (idPersonagem, sanidade, fome) VALUES (5, 50, 50);
+INSERT INTO Npc (idPersonagem, eBoss, tipoNpc) VALUES (3, true, 'N');
+
+
