8aa0e5d144c855f7f06d7efeaa4e1425104ff345
Luis Bruno <l.brunofidelis@gmail.com>
2023-12-10 11:20:00 -0300

    Adiciona exibição de informações no console

--- jogo/src/game.js
+++ jogo/src/game.js
@@ -3,18 +3,37 @@ import { buscarInstancia } from './instanciaPc/instanciaPc.js';
 import { buscarBiomaMundo } from './bioma/bioma.js';
 import { pressioneEnter } from './utils/console.js';
 import { buscarInstanciasNpc } from './instanciaNpc/instanciaNpc.js';
+import { buscarInstanciaItens } from './instanciaItem/instanciaItem.js';
+import { exibirMenu } from './menu.js';
 
 async function game(nomeUsuario, idPersonagemJogavel) {
   const instanciaPc = await buscarInstancia(nomeUsuario, idPersonagemJogavel);
-  
+
   const biomaMundo = await buscarBiomaMundo(instanciaPc.idmundo, instanciaPc.idbioma);
   console.log(chalk.blue(biomaMundo.nomemundo, "  -  ", biomaMundo.nomeestacao, "  -  ", "Dia: ", biomaMundo.diaatual));
   console.log(chalk.blue('Você está em um(a) ' + biomaMundo.nomebioma, ', ' + biomaMundo.descricao));
 
-  console.log(chalk.white('Neste(a) ', biomaMundo.nomemundo, ' possui:'));
+  console.log(chalk.white('\nNeste(a) ', biomaMundo.nomebioma, ' possui:'));
+  console.log('NPCs:');
   const instanciasNpc = await buscarInstanciasNpc(instanciaPc.idmundo, instanciaPc.idbioma);
-  console.log(instanciasNpc);
+  
+  instanciasNpc.forEach((instanciaNpc) => {
+    if(instanciaNpc.tipoNpc === 'H') {
+      console.log(chalk.red("  • ", instanciaNpc.nomepersonagem, ' - ', instanciaNpc.descricaopersonagem));
+    } else {
+      console.log(chalk.green("  • ", instanciaNpc.nomepersonagem, ' - ', instanciaNpc.descricaopersonagem));
+    }
+  });
+
+  const instanciasItens = await buscarInstanciaItens(instanciaPc.idmundo, instanciaPc.idbioma);
+  console.log('Itens:');
+  instanciasItens.forEach((instanciaItem) => {
+    console.log(chalk.green("  • ", instanciaItem.nome, `(x${instanciaItem.quantidade})`, ' - ', instanciaItem.descricao));
+  });
+
   await pressioneEnter('Pressione Enter para continuar');
+
+  await exibirMenu();
 }
 
 async function imprimeLocalizacao(nomeUsuario, idPersonagemJogavel) {
