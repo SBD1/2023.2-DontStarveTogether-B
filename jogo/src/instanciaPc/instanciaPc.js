3f7511d8dc847b7aac47e7258cef7a309dcf3b7c
Luis Bruno <l.brunofidelis@gmail.com>
2023-12-10 19:16:39 -0300

    Adiciona queries de busca instanciaPc

--- jogo/src/instanciaPc/instanciaPc.js
+++ jogo/src/instanciaPc/instanciaPc.js
@@ -3,15 +3,37 @@ import db from '../db/db.js';
 export async function buscarInstancia(nomeUsuario, idPersonagemJogavel) {
   try {
     const query = `
-    SELECT *
-    FROM InstanciaPc
-    WHERE nomeUsuario = $1 AND idPersonagemJogavel = $2
+      SELECT
+        IPC.*,
+        PC.sanidade,
+        PC.fome,
+        P.vida,
+        P.dano,
+        P.nome
+      FROM
+        InstanciaPC IPC
+      JOIN
+        PersonagemJogavel PC ON IPC.idPersonagemJogavel = PC.idPersonagem
+      JOIN
+        Personagem P ON PC.idPersonagem = P.id
+      WHERE
+        IPC.nomeUsuario = $1 AND IPC.idPersonagemJogavel = $2
     `;
     
     const instanciaPc = await db.oneOrNone(query, [nomeUsuario, idPersonagemJogavel]);
     return instanciaPc;
   } catch (error) {
     console.error('Erro ao buscar instância:', error);
+    throw error;
+  }
+}
+
+export async function buscarInstanciaPcPorId(idInstanciaPc) {
+  try {
+    const instanciaPc = await db.oneOrNone("SELECT * FROM InstanciaPc WHERE id = $1", [idInstanciaPc]);
+    return instanciaPc;
+  } catch (error) {
+    console.error("Erro ao buscar instância Pc:", error);
     throw error;
   }
 }
